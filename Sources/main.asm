5b39556a93742ccbef296ef76df47b3547108879
Seth Forslund <47567309+forslu@users.noreply.github.com>
2023-12-06 17:48:43 -0500

    rework-lab6_3(v2)-4_28_21

--- Sources/main.asm
+++ Sources/main.asm
@@ -10,191 +10,59 @@
             INCLUDE 'derivative.inc'
 
 ; export symbols
-            XDEF Entry, _Startup, main, 
-            XREF __SEG_END_SSTACK, read_pot,  RTI_ISR, sum, hexval, JUMPASS   
+            XDEF Entry, _Startup
+            ; we use export 'Entry' as symbol. This allows us to
+            ; reference 'Entry' either in the linker .prm file
+            ; or from C/C++ later on
+
+            XREF __SEG_END_SSTACK      ; symbol defined by the linker for the end of the stack
             
-            
-            
-   ;potentiometer.c.o was created auto????         
+            ; LCD References
+	         
 
-MY_EXTENDED_RAM: SECTION
-password  dc.b  $EB, $77, $7B, $7D
-
-
-myvars:   section
-passtemp  ds.b  4
+            ; Potentiometer References
+          
 
 
+
+; variable/data section
+my_variable: SECTION
+disp:	ds.b 33
+
+;constant section
+my_constant: SECTION
+constant_string dc.b    'The value of the pot is:        ',0
+
+
+; code section
 MyCode:     SECTION
-main:
-_Startup:
 Entry:
-            LDS  #__SEG_END_SSTACK     ; initialize the stack pointer
-            CLI                     ; enable interrupts
-            
+_Startup:
 
-;START
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Start:      ldx  #0
-
-GetPass:    ;set password for start
-            ;call LCD to display "enter password"
-            jsr  JUMPASS     ;call keypad for password
-            ldaa hexval     ;load keypad value  to a
-            staa passtemp, x   ;store keypad val to passtemp
-            inx              ;
-            cpx  #4
-            bne  GetPass
-            
-            ldx #0
-;compare password with entry          
-CompASS:    cpx  #4
-            beq  GoodPass
-            ldaa passtemp, x  ;this or do y+
-            ldab password, x  ;and x
-            inx
-            cba
-            beq CompASS
-            bra BadPass
-            
-GoodPass:   ;WIP//jsr HappySound                  ;speaker for correct
-            bra Push2strt
-                      
-BadPass:    ;WIP//jsr SadSound                  ;speaker for incorrect 
-            ;WIP//call LCD to display "wrong password"
-            lbra Start          
-                      ;return to start 
-Push2strt:            ;WIP//call LCD to display "press button to start"
-                      ;//WIP push to start
-                     
-;POT
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-pot:
-    jsr read_pot  ;jsr read pot value
-                
-                ;sort alg:     
-                          ;is potval < or >= 31
-                             ;<
-                                  ;bne 0, 15mph 
-                                    ;(potval=0) ton = 0 toff = 15
-                                    ;bra pot
-                                  
-                                  ;15mph: ton = 4 toff = 11  
-                                    ;bra pot
-                                  
-                             ;>=
-                                  ;is potval < or >= 61
-                                  
-                                    ;<
-                                        ;ton = 8 toff = 7
-                                        ;bra pot
-                                
-                                    ;>=
-                                        ;potval > 90
-                                          ;ton = 15 toff = 0
-                                          ;bra pot
-                                          
-                                        ;else (potval <= 90)
-                                          ;ton = 12 toff = 3
-                                          ;bra pot
-                                        
-                                    
-                      
-;DC MOTOR
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-      ;set interrupt freq to 1 second
-      ;collects mileage  
+	lds #__SEG_END_SSTACK			;initializes stack pointer	
+	ldx #constant_string			;loads arguments into registers
+	ldy #disp 
+	jsr string_copy				;passes arguments to string copy subroutine
 
 
 
 
 
-;IRQ
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                      
-   ;jsr keypadcrash   
-      
-      
-      
-;KEYPAD
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ;password ds.b 4    01, 02, 03, 04
- ;keypad input into a load first byte of password into b
- ;cba (compare b to a)
- ;continue for rest 
+;**************************************************************
+;
+;                   Write You Code Here
+;
+;**************************************************************	
+; tip: do not let your code fall into the subroutine below this line
 
 
- ;keypadcrash:      
-                    ;enter crash password
-                    
+string_copy: pshb
+	     ldab #33					;copies string starting at the first argument into the string starting at the second argument element-by-element
+   cpy_loop: ldaa 1,x+					;uses x and y to store pointers
+	     staa 1,y+
+	     decb
+	     bne cpy_loop				;repeats if b != 0
+	     pulb
+             rts					;pull and return
 
 
-;SWITCHES
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
-
-
-
-
-;TURNING
-;__________________________________________________________________________________
-
-;STEPPER MOTOR
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-;LED
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-     ;turn signal
-
-;____________________________________________________________________________________
-
-
-
-;LCD
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-
-
-
-
-
-
-
-;PUSH BUTTON
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-
-;SPEAKER
-;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-                      
-                      
